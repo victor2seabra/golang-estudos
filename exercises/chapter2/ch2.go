6238fc2d0779e91955f909b0e47fbc91066a9fae
victor-seabra <victor.seabra@aleon.ai>
2025-04-27 20:12:33 -0300

    add net/http library

--- exercises/chapter2/ch2.go
+++ exercises/chapter2/ch2.go
@@ -2,18 +2,15 @@ package teste
 
 import "fmt"
 
-// definindo explicitamente variáveis
-// global scope
+// definindo explicitamente variáveis global scope
 const x int64 = 10 // uma constante nao pode ter seu valor alterado
 
 // somente funções e outros objetos iniciados com letra maiscula podem ser importados
 func Teste() {
-	// definindo variaveis com tipagem explicita dentro de funções
-	// local scope
+	// definindo variaveis com tipagem explicita dentro de funções local scope
 	var y float64 = 5.5
 
-	// definindo variável com tipagem implicita
-	// possivel somente dentro de funções
+	// definindo variável com tipagem implicita possivel somente dentro de funções
 	z := 5
 
 	fmt.Printf("O valor de x é %d.\nO valor de y é %.2f.\nO valor de z é %d\n", x, y, z)
@@ -31,8 +28,7 @@ func Teste() {
 		fmt.Println("Zero")
 	}
 
-	// estrutura de um for loop
-	// inicialização; condição; após a condição validar
+	// estrutura de um for loop: <inicialização>; <condição>; <após a condição validar>
 	for b := y; b > float64(z); b++ {
 		fmt.Printf("%v", b)
 	}
